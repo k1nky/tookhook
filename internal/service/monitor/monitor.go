4ebb30edda4948f4f834dd8109b34ee2f22a1fb7
Andrey Shalashov <avshalashov@yandex.ru>
2024-07-26 20:50:02 +0300

    add comments

--- internal/service/monitor/monitor.go
+++ internal/service/monitor/monitor.go
@@ -1,3 +1,4 @@
+// Package monitor defines a service for monitoring the status of internal components of the tookhook server.
 package monitor
 
 import (
@@ -6,6 +7,7 @@ import (
 	"github.com/k1nky/tookhook/internal/entity"
 )
 
+// Service for monitoring the status of internal components of the tookhook server.
 type Service struct {
 	hookerSvc hookService
 	log       logger
@@ -13,6 +15,7 @@ type Service struct {
 	store     storage
 }
 
+// Return new instance of service.
 func New(pm pluginmanager, hookerSvc hookService, store storage, log logger) *Service {
 	return &Service{
 		hookerSvc: hookerSvc,
@@ -22,11 +25,13 @@ func New(pm pluginmanager, hookerSvc hookService, store storage, log logger) *Se
 	}
 }
 
+// Return the current status of the tookhook server.
 func (svc *Service) Status(ctx context.Context) entity.ServiceStatus {
 	status := svc.hookerSvc.Health(ctx)
 	pluginsStatus := svc.pm.Health(ctx)
 	for _, v := range pluginsStatus {
 		if v == entity.StatusFailed {
+			// mark the server as failed if there is any failed plugin
 			status = entity.StatusFailed
 			break
 		}
